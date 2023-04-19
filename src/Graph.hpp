c4e6aac98325a01ce835f196db1cf9253a0b1009
Vinicius Gabriel <viniciusgabriel7711@gmail.com>
2023-04-18 23:21:06 -0300

    improved initial graph implementation

--- src/Graph.hpp
+++ src/Graph.hpp
@@ -2,14 +2,18 @@
 //#include <unordered_set>
 #include <ostream>
 
+#define DEFAULT_DENSITY 30
+
 /// @brief Simple non-directed graph adjacency list implementation
 class Graph
 {
 private:
     int start, n, m;
     std::vector<std::vector<int>> adjList;
+    void genMinConnectedGraph();
 public:
     Graph(int start_, int n_, int density);
+    Graph(std::string filename);
     ~Graph();
     int getN();
     int getM(); 
@@ -17,5 +21,7 @@ public:
     void addVertex();
     void removeEdge(int v, int w);
     void removeVertex(int v);
+    bool isConnected();
+    void dump(std::string filename);
     friend std::ostream &operator<<(std::ostream &os, Graph g);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <cstdlib>
 #include "Graph.hpp"
 
-// Test to generate and print a graph with the given parameters.
 int main(int argc, char *argv[]) 
 {
-    if (argc != 4) return 1;
-    Graph g(atoi(argv[1]), atoi(argv[2]),  atoi(argv[3]));
-    std::cout << "m = " << g.getM() << "\n" << g;
+    Graph g("./tests/graph-test-8.txt");
+    std::cout << g << "\n";
     return 0;
 }
